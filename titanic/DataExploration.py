0b667dc532594f04aeaa1ea3988752efd538c269
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-08-19 08:47:13 -0400

    Find correlation between age category and survival rate.

--- titanic/DataExploration.py
+++ titanic/DataExploration.py
@@ -1,10 +1,9 @@
 import DataImport as di
 import matplotlib.pyplot as plt
 
-train_data = di.get_titanic_data()
-
 
 def plot_survival_by_gender():
+    train_data = di.get_titanic_data()
     gender_table = train_data.pivot_table(index="Sex", values="Survived")
     gender_table.plot.bar()
     plt.title("Graph to Show Survival Rate by Gender")
@@ -14,6 +13,7 @@ def plot_survival_by_gender():
 
 
 def plot_survival_by_class():
+    train_data = di.get_titanic_data()
     pclass_table = train_data.pivot_table(index="Pclass", values="Survived")
     pclass_table.plot.bar()
     plt.title("Graph to Show Survival Rate by Class")
@@ -23,6 +23,7 @@ def plot_survival_by_class():
 
 
 def plot_survival_by_age():
+    train_data = di.get_titanic_data()
     survived = train_data[train_data["Survived"] == 1]
     died = train_data[train_data["Survived"] == 0]
     survived["Age"].plot.hist(alpha=0.5, color="green", bins=50)
@@ -32,3 +33,21 @@ def plot_survival_by_age():
     plt.ylabel("Survival Rate")
     plt.legend(["Survived", "Died"])
     plt.savefig("Graph to Show Survival Rate by Age")
+
+
+def plot_survival_by_age_category(cuts: list, labels: list):
+    train_data = di.get_titanic_data()
+    train_data = di.filter_age(train_data, cuts, labels)
+    age_categories_pivot = train_data.pivot_table(index="Age_category", values="Survived")
+    age_categories_pivot.plot.bar()
+    plt.title("Graph to Show Survival Rate by Age Category")
+    plt.xlabel("Age Group")
+    plt.ylabel("Survival Rate")
+    plt.savefig("Graph to Show Survival Rate by Age Category")
+
+
+# cuts = [-1, 0, 5, 12, 18, 35, 60, 100]
+# label_names = ["Missing", 'Infant', "Child", 'Teenager', "Young Adult", 'Adult', 'Senior']
+# plot_survival_by_age_category(cuts, label_names)
+
+# plot_survival_by_age()
