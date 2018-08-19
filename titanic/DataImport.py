17027fb421bcc9d73a110f9a6cb28de676dd3715
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-08-19 08:48:06 -0400

    Clean Titanic dataset.

--- titanic/DataImport.py
+++ titanic/DataImport.py
@@ -5,14 +5,6 @@ TEST_FILE_NAME = "test.csv"
 GENDER_DATA_FILE_NAME = "gender_submission.csv"
 
 
-def get_titanic_train_x_data() -> pd.DataFrame:
-    return get_titanic_data().drop('Survived', axis=1)
-
-
-def get_titanic_train_y_data() -> pd.DataFrame:
-    return get_titanic_data()['Survived']
-
-
 def get_titanic_data() -> pd.DataFrame:
     return pd.read_csv(TRAIN_FILE_NAME)
 
@@ -23,3 +15,41 @@ def get_titanic_test_data() -> pd.DataFrame:
 
 def get_gender_data() -> pd.DataFrame:
     return pd.read_csv(GENDER_DATA_FILE_NAME)
+
+
+def filter_age(data: pd.DataFrame, cuts: list, labels: list) -> pd.DataFrame:
+    data['Age'] = data['Age'].fillna(-0.5)
+    data['Age_category'] = pd.cut(data['Age'], cuts, labels=labels)
+    return data
+
+
+def create_dummies(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    dummies = pd.get_dummies(data[column], prefix=column)
+    return pd.concat([data, dummies], axis=1)
+
+
+def clean_titanic_data(data: pd.DataFrame, age_cuts:list, age_labels: list):
+    data = filter_age(data, age_cuts, age_labels)
+    data = create_dummies(data, 'Pclass')
+    data = create_dummies(data, 'Age_category')
+    data = create_dummies(data, 'Sex')
+    return data
+
+
+def get_clean_train_data(age_cuts: list, age_labels: list):
+    # TODO: Use builder notation so this becomes easier
+    train_data = get_titanic_data()
+    train_data = clean_titanic_data(train_data, age_cuts, age_labels)
+    return train_data
+
+
+def get_clean_test_data(age_cuts: list, age_labels: list):
+    test_data = get_titanic_test_data()
+    test_data = clean_titanic_data(test_data, age_cuts, age_labels)
+    return test_data
+
+
+cuts = [-1, 0, 5, 12, 18, 35, 60, 100]
+label_names = ["Missing", 'Infant', "Child", 'Teenager', "Young Adult", 'Adult', 'Senior']
+x = get_clean_train_data(cuts, label_names)
+print(x.columns)
