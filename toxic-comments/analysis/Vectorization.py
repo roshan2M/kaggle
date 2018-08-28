3f8e563e66e5a79a070ddc256b51c17ddcc93548
Roshan Munjal <roshan.munjal@hotmail.com>
2018-08-28 15:39:07 -0400

    Fix vectorization

--- toxic-comments/analysis/Vectorization.py
+++ toxic-comments/analysis/Vectorization.py
@@ -25,7 +25,7 @@ def get_comment_vectors(toxic_comments_data):
     comment_vectorizer = cv.CommentVectorizer()
     vectorizer = comment_vectorizer.get_count_vectorizers()
     comment_vectorizer.fit(toxic_comments_data[di.TEXT_COLUMN], vectorizer)
-    return comment_vectorizer.transform(toxic_comments_train[di.TEXT_COLUMN], vectorizer)
+    return comment_vectorizer.transform(toxic_comments_data[di.TEXT_COLUMN], vectorizer)
 
 
 def plot_reduced_comment_vectors(comment_vectors):
@@ -37,9 +37,9 @@ def plot_reduced_comment_vectors(comment_vectors):
     plt.savefig("2D Plot of Reduced Comment Vectors (using SVD).pdf")
 
 
-toxic_comments_train = load_train_data()
-get_basic_properties(toxic_comments_train)
-
-comment_vectors = get_comment_vectors(toxic_comments_train)
-
-plot_reduced_comment_vectors(comment_vectors)
+# toxic_comments_train = load_train_data()
+# get_basic_properties(toxic_comments_train)
+#
+# comment_vectors = get_comment_vectors(toxic_comments_train)
+#
+# plot_reduced_comment_vectors(comment_vectors)
